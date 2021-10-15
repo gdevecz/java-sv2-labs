ab4b5670aab8f78c4b5fb1232fd46865ac6b5e24
gdevecz <gdevecz@gmail.com>
2021-10-15 16:17:34 +0200

    Gyakorlati feladat - Bankszámlák megoldása

---
+++ training-solutions/src/main/java/classstructureintegrate/Bank.java
@@ -0,0 +1,32 @@
+package classstructureintegrate;
+
+import java.util.Scanner;
+
+public class Bank {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Name of account owner: ");
+        String owner = sc.nextLine();
+        System.out.print("Account number: ");
+        String accountNumber=sc.nextLine();
+        System.out.print("Balance: ");
+        int balance=sc.nextInt();
+
+        BankAccount bankAccount = new BankAccount(accountNumber, owner, balance);
+
+        System.out.println("The attributes of the account: " + bankAccount.getInfo());
+        System.out.println();
+
+        System.out.print("Amount of decrease: ");
+        bankAccount.withdraw(sc.nextInt());
+        System.out.println(bankAccount.getInfo());
+
+        sc.nextLine();System.out.print("Amount of increase: ");
+        bankAccount.deposit(sc.nextInt());
+        sc.nextLine();
+        System.out.println(bankAccount.getInfo());
+    }
+}
