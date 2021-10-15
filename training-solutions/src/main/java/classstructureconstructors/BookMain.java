4e1567f2a734e83d304e95111ba82afa8a5e71ff
gdevecz <gdevecz@gmail.com>
2021-10-15 16:14:03 +0200

    Gyakorlati feladat - Könyv regisztráció megoldása

---
+++ training-solutions/src/main/java/classstructureconstructors/BookMain.java
@@ -0,0 +1,14 @@
+package classstructureconstructors;
+
+public class BookMain {
+
+    public static void main(String[] args) {
+        Book book = new Book("Rejtő Jenő", "Előretolt helyőrség");
+        book.register("RJ012");
+
+        System.out.println("The book: ");
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Registration number: " + book.getRegNumber());
+    }
+}
