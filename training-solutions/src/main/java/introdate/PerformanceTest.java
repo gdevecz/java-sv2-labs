dab75dadbd5addf2429755b4a0e704e4f1bc5a21
gdevecz <gdevecz@gmail.com>
2021-10-22 00:32:43 +0200

    Gyakorlati feladat - Előadók fellépései javítás adatkiiratás sout-okkal.

--- training-solutions/src/main/java/introdate/PerformanceTest.java
+++ training-solutions/src/main/java/introdate/PerformanceTest.java
@@ -5,6 +5,11 @@ public class PerformanceTest {
     public static void main(String[] args) {
         Performance performance = new Performance("Queen", 1989, 06, 02, 18, 20);
 
+        System.out.println("Artist: " + performance.getArtist());
+        System.out.println("Date of performance: " + performance.getDateOfShow());
+        System.out.println("Start: " + performance.getStartTime());
+        System.out.println("End: " + performance.getEndTime());
+
         System.out.println(performance.getInfo());
     }
 }
