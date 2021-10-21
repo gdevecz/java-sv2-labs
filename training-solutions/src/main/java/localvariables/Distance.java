85bd70386281fd436c2f09f8a46ec7833360e03e
gdevecz <gdevecz@gmail.com>
2021-10-21 14:29:51 +0200

    Gyakorlati feladat - Távolság

--- training-solutions/src/main/java/localvariables/Distance.java
+++ training-solutions/src/main/java/localvariables/Distance.java
@@ -1,11 +1,11 @@
 package localvariables;
 
-public class Distances {
+public class Distance {
 
     private double distanceInKm;
     private boolean exact;
 
-    public Distances(double distanceInKm, boolean exact) {
+    public Distance(double distanceInKm, boolean exact) {
         this.distanceInKm = distanceInKm;
         this.exact = exact;
     }
--- training-solutions/src/main/java/localvariables/DistancesMain.java
+++ training-solutions/src/main/java/localvariables/DistancesMain.java
@@ -1,4 +1,29 @@
 package localvariables;
 
+import java.util.Scanner;
+
 public class DistancesMain {
+
+    public static void main(String[] args) {
+
+        Distance distance;
+
+        double distanceKm;
+
+        boolean exact;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Distance: ");
+        distanceKm = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Is the distance exact (true/false)? ");
+        exact = sc.nextBoolean();
+        distance = new Distance(distanceKm, exact);
+
+        System.out.println("The distance is " + (distance.isExact() ? "" : "not")
+                + " exact " + distance.getDistanceInKm() + "Km");
+        int i = (int) distance.getDistanceInKm();
+        System.out.println("The distance as Integer: " + i);
+    }
+
 }
