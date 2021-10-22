9aae50870beebb7cd52b2d2db632183ed7d04a0e
gdevecz <gdevecz@gmail.com>
2021-10-22 19:31:23 +0200

    Gyakorlati feladat - Számsorozat apróbb módosítás

--- training-solutions/src/main/java/aslist/Sequence.java
+++ training-solutions/src/main/java/aslist/Sequence.java
@@ -10,12 +10,14 @@ public class Sequence {
     public static void main(String[] args) {
         List<Integer> elements = new ArrayList<>(Arrays.asList(1 ,2));
         Scanner sc = new Scanner(System.in);
+        int numberOfAddElements = 5;
 
         System.out.println("Kérem a listához adandó számokat: ");
-        for (int i = elements.size(); i < 7; i++) {
+        numberOfAddElements+=elements.size();
+        for (int i = elements.size(); i < numberOfAddElements; i++) {
             elements.add(sc.nextInt());
         }
-        System.out.println("A listában szerplő " + elements.size() + " szám: ");
+        System.out.println("A listában szereplő " + elements.size() + " szám: ");
         System.out.println(elements);
     }
 }
