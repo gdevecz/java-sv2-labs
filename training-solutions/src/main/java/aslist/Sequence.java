655baa5f8f584c189ea27dcae32b2307732f0efa
gdevecz <gdevecz@gmail.com>
2021-10-22 19:22:24 +0200

    Gyakorlati feladat - Számsorozat

---
+++ training-solutions/src/main/java/aslist/Sequence.java
@@ -0,0 +1,21 @@
+package aslist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+        List<Integer> elements = new ArrayList<>(Arrays.asList(1 ,2));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Kérem a listához adandó számokat: ");
+        for (int i = elements.size(); i < 7; i++) {
+            elements.add(sc.nextInt());
+        }
+        System.out.println("A listában szerplő " + elements.size() + " szám: ");
+        System.out.println(elements);
+    }
+}
