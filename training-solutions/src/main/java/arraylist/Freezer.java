238ee5d0dbcc94bb8ce835d72f7c9e9000686ea0
gdevecz <gdevecz@gmail.com>
2021-10-22 18:31:19 +0200

    Gyakorlati feladat - A mélyhűtő tartalma

---
+++ training-solutions/src/main/java/arraylist/Freezer.java
@@ -0,0 +1,23 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Freezer {
+
+    public static void main(String[] args) {
+
+        List<String> listOfItems = new ArrayList<>();
+        listOfItems.add("Sör");
+        listOfItems.addAll(Arrays.asList("Tej", "Szalámi", "Üdítő", "Margarin"));
+
+        System.out.println(listOfItems.size());
+        System.out.println(listOfItems);
+        listOfItems.remove(1);
+        listOfItems.remove("Üdítő");
+        System.out.println(listOfItems.size());
+        System.out.println(listOfItems);
+
+    }
+}
---
+++ training-solutions/src/main/java/arraylist/Main.java
@@ -0,0 +1,14 @@
+package arraylist;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("Sör", "Tej", "Szalámi", "Üdítő", "Margarin");
+        Freezer freezer = new Freezer();
+        ;
+
+    }
+}
