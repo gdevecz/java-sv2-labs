91af54c7cc6731734e8d62faf334ab97636a9de8
gdevecz <gdevecz@gmail.com>
2021-10-23 00:03:26 +0200

    Gyakorlati feladat - A Math osztály kipróbálása

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,22 @@
+package math.math;
+
+public class MathMethods {
+
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,9));
+        System.out.println(Math.min(5,9));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.round(3.7));
+        System.out.println(Math.abs(-3.7));
+        System.out.println(Math.negateExact(1));
+        System.out.println(Math.addExact(3,5));
+        System.out.println(Math.subtractExact(3,5));
+        System.out.println(Math.multiplyExact(4,3));
+        System.out.println(Math.pow(2.3,4.1));
+        System.out.println(Math.incrementExact(7));
+        System.out.println(Math.decrementExact(7));
+        System.out.println(Math.random());
+
+    }
+}
