2130836b78d8ba416f4f0a5e97ffb54c5e0cecb2
gdevecz <gdevecz@gmail.com>
2021-10-26 22:24:45 +0200

    Gyakorlati feladat - Regisztráció isValidUsername == -> != volt egy kis gondom, az állományokkal, és ez így maradt

--- training-solutions/src/main/java/stringtype/registration/UserValidator.java
+++ training-solutions/src/main/java/stringtype/registration/UserValidator.java
@@ -3,7 +3,7 @@ package stringtype.registration;
 public class UserValidator {
 
     public boolean isValidUsername(String username) {
-        return username.length() == 0;
+        return username.length() != 0;
     }
 
     public boolean isValidPassword(String password1, String password2) {
