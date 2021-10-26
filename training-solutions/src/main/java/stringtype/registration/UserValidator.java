864e9606fdf84c2a3c2a7e1f53b0f636fe302039
gdevecz <gdevecz@gmail.com>
2021-10-26 12:41:05 +0200

    felesleges ?: kiszedése;

--- training-solutions/src/main/java/stringtype/registration/UserValidator.java
+++ training-solutions/src/main/java/stringtype/registration/UserValidator.java
@@ -3,16 +3,16 @@ package stringtype.registration;
 public class UserValidator {
 
     public boolean isValidUsername(String username) {
-        return username.length() == 0 ? false : true;
+        return username.length() == 0;
     }
 
     public boolean isValidPassword(String password1, String password2) {
-        return (!(password1.equals(password2)) || password1.length() < 8) ? false :  true;
+        return (!(password1.equals(password2)) || password1.length() < 8);
     }
 
     public boolean isValidEmail(String email) {
         int atIndex = email.indexOf('@');
         int dotIndex = email.indexOf('.',atIndex + 1);
-        return (atIndex < 0 || dotIndex == email.length() - 1 || atIndex + 1 > dotIndex) ? false : true;
+        return (atIndex < 0 || dotIndex == email.length() - 1 || atIndex + 1 > dotIndex);
     }
 }
