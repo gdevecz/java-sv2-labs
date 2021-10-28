985cbb553acafea5d06e9f06db9833fb5a076924
gdevecz <gdevecz@gmail.com>
2021-10-28 15:24:12 +0200

    Gyakorlati feladat - Egy könyv és a címe még egy teszt hozzáadása (anotherBook)

--- training-solutions/src/main/java/attributes/book/BookMain.java
+++ training-solutions/src/main/java/attributes/book/BookMain.java
@@ -4,9 +4,12 @@ public class BookMain {
 
     public static void main(String[] args) {
         Book book = new Book("Piszkos Fred a kapitány");
-
+        Book anotherBook = new Book("Galaxis útikalauz stopposoknak");
         System.out.println(book.getTitle());
         book.setTitle("Előretolt helyőrség");
         System.out.println(book.getTitle());
+        System.out.println(anotherBook.getTitle());
+        anotherBook.setTitle("Az Élet, a Világmindenség meg Minden");
+        System.out.println(anotherBook.getTitle());
     }
 }
