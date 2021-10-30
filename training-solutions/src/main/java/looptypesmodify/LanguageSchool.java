96cf9a6c612679cc3fdd07f79cedf0698e052474
gdevecz <gdevecz@gmail.com>
2021-10-30 21:49:07 +0200

    Gyakorlati feladat - Nyelviskola apróbb módosítások

--- training-solutions/src/main/java/looptypesmodify/LanguageSchool.java
+++ training-solutions/src/main/java/looptypesmodify/LanguageSchool.java
@@ -19,15 +19,23 @@ public class LanguageSchool {
         students.get(2).setActive(false);
         students.get(4).setActive(false);
 
+        System.out.println("A tanulók listája:");
+        for (Student student : students) {
+            System.out.println(student.getName() + "státusz: " + student.isActive());
+        }
+
         List<Student> removableStudents = new ArrayList();
         for (Student student : students) {
             if (!student.isActive()) {
                 removableStudents.add(student);
             }
         }
-//        A student instance-ba MÁSOLÓDIK a students éppen aktuális eleme(itt referencia) (mint a paraméterlistában)
-//        primitív típusokat így ne tudok megváltoztatni (nem írja vissza a listába), de referencián kersztül
-//        módosíthatók az adatok(mint a paraméterlistában)
+        students.removeAll(removableStudents);
+
+//          foreach
+//        A student változóba/zsebbe/instance-be MÁSOLÓDIK a students éppen aktuális eleme(itt referencia) (mint a paraméterlistában)
+//        primitív típusokat így nem tudok megváltoztatni (nem írja vissza a listába), de referencián keresztül
+//        módosíthatók az adatok(mint a paraméterlistában, másolat)
 //        System.out.println(students.get(2));
 //        System.out.println(removableStudents.get(0));
 //        System.out.println(students.get(4));
@@ -37,8 +45,11 @@ public class LanguageSchool {
 //        for (Student student : students) {
 //            student.setName("dr. " + student.getName());
 //        }
+
+        System.out.println();
+        System.out.println("A tanulók listája:");
         for (Student student : students) {
-            System.out.println(student.getName());
+            System.out.println(student.getName() + "státusz: " + student.isActive());
         }
     }
 }
