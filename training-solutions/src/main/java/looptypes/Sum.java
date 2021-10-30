a0b57533c2e793a48d110b8ff368c56ff534b087
gdevecz <gdevecz@gmail.com>
2021-10-30 21:33:28 +0200

    Gyakorlati feladat - Számok összege psvm-hez csináltam maint, a többi nem változott

--- training-solutions/src/main/java/looptypes/Sum.java
+++ training-solutions/src/main/java/looptypes/Sum.java
@@ -2,12 +2,6 @@ package looptypes;
 
 public class Sum {
 
-    public static void main(String[] args) {
-        Sum sum = new Sum();
-
-        sum.printSums(new int[]{2, 6, 3, 5, 7, 9});
-    }
-
     public void printSums(int[] numbers) {
         for (int i = 0; i < numbers.length - 1; i++) {
             System.out.print(numbers[i] + numbers[i + 1] + " ");
---
+++ training-solutions/src/main/java/looptypes/SunMain.java
@@ -0,0 +1,10 @@
+package looptypes;
+
+public class SunMain {
+
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+
+        sum.printSums(new int[]{2, 6, 3, 5, 7, 9});
+    }
+}
