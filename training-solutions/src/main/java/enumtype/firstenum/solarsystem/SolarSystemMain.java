f56268a58cdf785e9d4a1d9969217d82bc044248
gdevecz <gdevecz@gmail.com>
2021-10-31 01:28:45 +0200

    Gyakorlati feladat - A Naprendszer bolygói attributum láthatóság javítása

--- training-solutions/src/main/java/enumtype/firstenum/solarsystem/SolarSystemMain.java
+++ training-solutions/src/main/java/enumtype/firstenum/solarsystem/SolarSystemMain.java
@@ -6,7 +6,7 @@ public class SolarSystemMain {
     public static void main(String[] args) {
         SolarSystem[] solarSystems = SolarSystem.values();
         for (int i = 0; i < solarSystems.length; i++) {
-            System.out.println("Planet: " + solarSystems[i].name() + ", number of moons: " + solarSystems[i].moons);
+            System.out.println("Planet: " + solarSystems[i].name() + ", number of moons: " + solarSystems[i].getMoons());
         }
     }
 }
