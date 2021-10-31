21ea11f33451fc2e069f8369020c12a83d6b45b1
gdevecz <gdevecz@gmail.com>
2021-10-31 17:13:59 +0100

    Gyakorlati feladat - Számok indexe

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -3,11 +3,19 @@ package mathproblems;
 public class Division {
 
     public void getDivisors(int number) {
-        for (int i = 1; i <= (number / 2); i ++) {
-            if(number % i == 0) {
+        for (int i = 1; i <= (number / 2); i++) {
+            if (number % i == 0) {
                 System.out.println(number + " osztója: " + i);
             }
         }
         System.out.println(number + " osztója: " + number);
+    }
+
+    public void getRightNumbers(int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] % i == 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
     }
 }
--- training-solutions/src/main/java/mathproblems/DivisionMain.java
+++ training-solutions/src/main/java/mathproblems/DivisionMain.java
@@ -6,5 +6,7 @@ public class DivisionMain {
 
         Division division = new Division();
         division.getDivisors(12);
+
+        division.getRightNumbers(new int[]{2, 5, 23, 56, -36, 45, 78, 14, -8, 4, 10});
     }
 }
