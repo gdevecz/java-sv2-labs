63b40a8d604397b49e73f6d091bb7921b6722893
gdevecz <gdevecz@gmail.com>
2021-11-01 14:32:23 +0100

    Gyakorlati feladat - Számsor konvertálása a tesztek bővítése

--- training-solutions/src/main/java/stringscanner/IntScannerMain.java
+++ training-solutions/src/main/java/stringscanner/IntScannerMain.java
@@ -6,7 +6,19 @@ public class IntScannerMain {
         IntScanner intScanner = new IntScanner();
 
         String test = "5;3;107;12;123;18;198";
-
         System.out.println(intScanner.convertInts(test));
+
+        test = "198";
+        System.out.println(intScanner.convertInts(test));
+
+        test = "5";
+        System.out.println(intScanner.convertInts(test));
+
+        test = "5;3;12;18";
+        System.out.println(intScanner.convertInts(test));
+
+        test = "";
+        System.out.println(intScanner.convertInts(test));
+
     }
 }
