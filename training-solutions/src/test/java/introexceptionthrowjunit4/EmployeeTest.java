1e8ef0f690b1df8cde29344a81040d3f910ab481
gdevecz <gdevecz@gmail.com>
2021-11-21 01:39:16 +0100

    csak gyakorlás

---
+++ training-solutions/src/test/java/introexceptionthrowjunit4/EmployeeTest.java
@@ -0,0 +1,14 @@
+package introexceptionthrowjunit4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeTest {
+
+//    @Test(expected = )
+//    public void ctestCreate() {
+//        new Employee("");
+//    }
+
+}
