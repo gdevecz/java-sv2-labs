100f2caab6f6be372a53eadf312db8a59e5b41df
gdevecz <gdevecz@gmail.com>
2021-11-24 21:44:21 +0100

    Gyakorlati feladat - Palindroma

--- training-solutions/src/test/java/recursion/PalindromeTest.java
+++ training-solutions/src/test/java/recursion/PalindromeTest.java
@@ -1,9 +1,8 @@
 package recursion;
 
-import algorithmstransformation.students.Person;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PalindromeTest {
 
