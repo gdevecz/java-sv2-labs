ac4f7e54eafa999d3960a94bb38efc18dd879fb4
gdevecz <gdevecz@gmail.com>
2021-11-24 13:28:00 +0100

    Gyakorlati feladat - Állatkert

--- training-solutions/src/test/java/algorithmsfilter/zoo/ZooTest.java
+++ training-solutions/src/test/java/algorithmsfilter/zoo/ZooTest.java
@@ -1,12 +1,11 @@
 package algorithmsfilter.zoo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ZooTest {
 
