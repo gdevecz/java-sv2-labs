bdd09da79aa5880105b97cbe87a3ba107eb2afeb
gdevecz <gdevecz@gmail.com>
2021-12-03 15:07:16 +0100

    Gyakorlati feladat - A táska tartalma

---
+++ training-solutions/src/main/java/objectclass/bag/SimpleBag.java
@@ -0,0 +1,47 @@
+package objectclass.bag;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleBag {
+
+    private List<Object> items = new ArrayList<>();
+
+    private int cursor = -1;
+
+    public SimpleBag() {
+    }
+
+    public void putItem(Object item) {
+        items.add(item);
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void beforeFirst() {
+        cursor = -1;
+    }
+
+    public boolean hasNext() {
+        return !(isEmpty() || cursor >= items.size() - 1);
+    }
+
+    public Object next() {
+        cursor++;
+        return items.get(cursor);
+    }
+
+    public boolean contains(Object item) {
+        return items.contains(item);
+    }
+
+    public int getCursor() {
+        return cursor;
+    }
+}
