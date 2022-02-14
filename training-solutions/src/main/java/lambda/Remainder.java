3a4171e50e15286429fc8ad064e7233172e0cc1d
gdevecz <gdevecz@gmail.com>
2022-02-14 06:58:21 +0100

    Gyakorlati feladat - Osztási maradékok

--- training-solutions/src/main/java/lambda/Remainder.java
+++ training-solutions/src/main/java/lambda/Remainder.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Remainder {
 
     public List<Integer> changeNumbersToRemainders(List<Integer> numbers, int divisor) {
-        numbers.replaceAll(number -> number % divisor);
+        numbers.replaceAll(n -> n % divisor);
         return numbers;
     }
 }
